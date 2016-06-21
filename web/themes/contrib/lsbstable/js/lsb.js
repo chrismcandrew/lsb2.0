2885137fbad3d378915e44de39c981e8bd6d8aa8
Christopher McAndrew <chris@csmcreative.com>
2016-06-20 20:29:05 -0400

    Add themes

---
+++ web/themes/contrib/lsbstable/js/lsb.js
@@ -0,0 +1,40 @@
+(function ($) {
+
+
+
+
+$(document).ready(function(){
+// 'use strict';
+
+
+  $("#block-lsbstable-main-menu ul").addClass('nav blog-nav');
+  $("#block-lsbstable-main-menu li").addClass('nav-link');
+  //$("#block-lotussalon-mainnavigation").addClass('navbar-fixed-top');
+  $(".form-text, .form-textarea, .form-search, .form-email, select").addClass('form-control');
+  // $(".form-submit").addClass('btn btn-default');
+  // $("img").addClass('img-fluid');
+  $("#block-lsbstable-mainnavigation ul").addClass('nav blog-nav');
+  $(".button").addClass('btn btn-primary');
+  // $("#fallback-page-menu").css("display","none");
+  $("#block-lsbstable-mainnavigation li").addClass('nav-link');
+  $("ul.price-list").addClass('list-group');
+  $("ul.price-list li").addClass('list-group-item');
+  $("ul.price-list span").addClass('price strong');
+$( 'a[ title="Facebook" ]').addClass('icon icon-facebook');
+$( 'a[ title="Instagram" ]').addClass('icon icon-instagram');
+});
+})(jQuery);
+
+
+
+// (function ($) {  
+
+//   Drupal.behaviors.lotussalon = {
+
+//     attach: function (context, settings) {            
+
+
+
+//   }};
+
+// })(jQuery);
