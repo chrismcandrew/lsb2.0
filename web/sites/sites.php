f8dbafb427cf50e0d89a6c11590a7beb01a28396
Christopher McAndrew <chris@csmcreative.com>
2016-06-21 11:54:09 -0400

    Updated .gitignore

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -54,6 +54,6 @@
  * @see https://www.drupal.org/documentation/install/multi-site
  */
   $sites = array(
-    'csmcreative.dev' => 'csmcreative.com',
-    'lotussalonbrielle.dev' => 'lotussalonbrielle.com',
+    'csmcreative.dev' => 'csmcreative.dev',
+    'lotussalonbrielle.dev' => 'lotussalonbrielle.dev',
  );
