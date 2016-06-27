eea8d16f1cb73bbeaa5c8d8d78164f23646848d0
Christopher McAndrew <chris@csmcreative.com>
2016-06-27 00:10:54 -0400

    Added metatag and backup_migrate to composer.json, fixed sites.php

--- web/sites/sites.php
+++ web/sites/sites.php
@@ -54,6 +54,6 @@
  * @see https://www.drupal.org/documentation/install/multi-site
  */
   $sites = array(
-    'csmcreative.dev' => 'csmcreative.localhost',
+    'csmcreative.localhost' => 'csmcreative.localhost',
     'lotussalonbrielle.com' => 'lotussalonbrielle.com',
  );
